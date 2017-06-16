e172a4d7c90a71f92966414b6d51ab7aa08f0e92
Jonathan Arnett <jonarnett90@gmail.com>
2017-06-16 14:20:40 -0400

    Moved org-mode bindings to their own "C-c o" prefix

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -26,10 +26,10 @@
 (global-set-key (kbd "C-c m d") 'magit-diff)
 (global-set-key (kbd "C-c m c") 'magit-checkout)
 ;; Org mode
-(global-set-key (kbd "C-c l") 'org-store-link)
-(global-set-key (kbd "C-c a") 'org-agenda)
-(global-set-key (kbd "C-c c") 'org-capture)
-(global-set-key (kbd "C-c b") 'org-iswitchb)
+(global-set-key (kbd "C-c o l") 'org-store-link)
+(global-set-key (kbd "C-c o a") 'org-agenda)
+(global-set-key (kbd "C-c o c") 'org-capture)
+(global-set-key (kbd "C-c o b") 'org-iswitchb)
 ;; Custom
 (global-set-key (kbd "C-x c") 'compile)
 (global-set-key (kbd "C-x w") 'whitespace-mode)
