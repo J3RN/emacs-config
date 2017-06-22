c6e4583e3deb131cf1142f055285118ae6bb3fb6
Jonathan Arnett <jonarnett90@gmail.com>
2017-06-22 10:04:18 -0400

    Updated packages

--- .emacs.d/elpa/rust-mode-20170531.1558/rust-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "rust-mode" "20170531.1558" "A major emacs mode for editing Rust source code" '((emacs "24.0")) :commit "77248135153a6c15cfc35ff9d5db550a5b1a8197" :url "https://github.com/rust-lang/rust-mode" :keywords '("languages"))
