52e64cb92064c9708cf49a67ac95fbf591bd3d54
Jonathan Arnett <jonarnett90@gmail.com>
2017-07-07 10:40:03 -0400

    Package update

--- .emacs.d/elpa/markdown-mode-20170706.1309/markdown-mode-autoloads.el
+++ .emacs.d/elpa/markdown-mode-20170706.1309/markdown-mode-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22878 20166
+;;;### (autoloads nil "markdown-mode" "markdown-mode.el" (22879 38141
 ;;;;;;  0 0))
 ;;; Generated autoloads from markdown-mode.el
 
