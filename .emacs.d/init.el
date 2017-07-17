7840f530dcb2de6f750140af7690bf8788b61a5f
Jonathan Arnett <jonarnett90@gmail.com>
2017-07-17 12:27:12 -0400

    Added some sane styling for C

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -86,6 +86,11 @@
 (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
 ;; Show trailing whitespace in code files
 (add-hook 'prog-mode-hook (lambda () (setq show-trailing-whitespace t)))
+;; Styling C
+(add-hook 'c-mode-common-hook (lambda () (setq c-default-style "linux"
+					       c-basic-offset 8
+					       tab-width 8
+					       indent-tabs-mode t)))
 
 ;;; Scroll one line at a time (less "jumpy" than defaults)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1))) ;; one line at a time
