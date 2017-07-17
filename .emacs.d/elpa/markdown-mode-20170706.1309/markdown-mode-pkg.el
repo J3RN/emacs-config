e96a94ae77e995afe0702946853467b63017457a
Jonathan Arnett <jonarnett90@gmail.com>
2017-07-17 12:19:48 -0400

    Package update; Added Ivy/Counsel/Swiper and Paradox

--- .emacs.d/elpa/markdown-mode-20170706.1309/markdown-mode-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "markdown-mode" "20170706.1309" "Major mode for Markdown-formatted text" '((emacs "24") (cl-lib "0.5")) :commit "a4ca2d08c695e16ceed603b3ac02206bc121b160" :url "http://jblevins.org/projects/markdown-mode/" :keywords '("markdown" "github flavored markdown" "itex"))
