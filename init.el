f0bcd280dfaf043aa6b74ef9d6cdb4de5ba5b8c1
Jonathan Arnett <jarnett@covermymeds.com>
2018-09-13 20:32:52 -0400

    Use locate-user-emacs-file instead of hardcoding

--- init.el
+++ init.el
@@ -69,7 +69,7 @@
 (use-package dashboard
   :init
   (setq dashboard-items '((projects . 5) (recents . 5)))
-  (setq dashboard-startup-banner "~/.emacs.d/128x128@2x.png")
+  (setq dashboard-startup-banner (locate-user-emacs-file "128x128@2x.png"))
   :config
   (dashboard-setup-startup-hook))
 
