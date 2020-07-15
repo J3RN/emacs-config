e95f95302b84801eb926686a91496920388f5683
Jonathan Arnett <jonarnett90@gmail.com>
2020-07-15 14:58:10 -0400

    Allow upcase-region and downcase-region
    They're useful 🤷

--- init.el
+++ init.el
@@ -105,7 +105,9 @@
   (electric-pair-mode)
   (setq dired-listing-switches "-alh")
   (setq save-abbrevs 'silently)
-  (setq whitespace-style '(face tabs trailing space-before-tab newline empty space-after-tab tab-mark)))
+  (setq whitespace-style '(face tabs trailing space-before-tab newline empty space-after-tab tab-mark))
+  (put 'downcase-region 'disabled nil)
+  (put 'upcase-region 'disabled nil))
 
 (use-package enh-ruby-mode
   :delight "💎"
