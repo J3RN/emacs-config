d79ea6e9919579a01b34cec9f7cdd0b732052638
Jonathan Arnett <jonarnett90@gmail.com>
2021-04-15 10:28:45 -0400

    ANSI colorize magit process output

--- elisp/git.el
+++ elisp/git.el
@@ -13,7 +13,13 @@
   :bind
   ("C-c g s" . magit-status)
   ("C-c g f" . magit-file-dispatch)
-  ("C-c g c" . magit-clone))
+  ("C-c g c" . magit-clone)
+  :hook
+  ;; This is a bit of a hack since the third argument that
+  ;; `after-change-functions' provides to `ansi-color-apply-on-region' is the
+  ;; length of the region changed, but the third parameter of
+  ;; `ansi-color-apply-on-region' is a flag as to whether to preserve sequences.
+  (magit-process-mode . (lambda () (add-hook 'after-change-functions 'ansi-color-apply-on-region nil 'local))))
 
 (provide 'git)
 ;;; git.el ends here
