4bc72fe81ec03dafb3ea9c1f81561702e698bd3c
Jonathan Arnett <jonarnett90@gmail.com>
2023-06-07 09:37:26 -0400

    Clarify comment about mouse-wheel-follow-mouse

--- init.el
+++ init.el
@@ -395,7 +395,7 @@
 ;;; Set sane scrolling
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)) ; One line at a time
       mouse-wheel-progressive-speed nil            ; Don't accelerate scrolling
-      mouse-wheel-follow-mouse t                   ; Scroll window under mouse
+      mouse-wheel-follow-mouse t                   ; Scroll window mouse is positioned over
       mouse-wheel-tilt-scroll t                    ; Allow sideways scrolling
       mouse-wheel-flip-direction t                 ; This will forever bother me now that I've though about it
       scroll-conservatively 100)                   ; Scroll one line at a time when point moves off screen
