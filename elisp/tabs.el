48009f39e4c5ecae6e0e35bbe68cb4892c2151e7
Jonathan Arnett <jonarnett90@gmail.com>
2023-11-22 12:37:12 -0500

    Add SVG tab icons

--- elisp/tabs.el
+++ elisp/tabs.el
@@ -3,7 +3,26 @@
 ;;; Code:
 
 (setq tab-bar-new-tab-choice 'dashboard-open)
+(define-icon tab-bar-new nil
+  `((image ,(locate-user-emacs-file (file-name-concat "images" "add.svg"))
+	   :ascent center)
+    (text " + "))
+  "Icon for creating a new tab."
+  ;; The version here is a bit silly
+  :scale 0.75
+  :version "29.1"
+  :help-echo "New tab")
+
 (setq tab-bar-close-last-tab-choice 'delete-frame)
+(define-icon tab-bar-close nil
+  `((image ,(locate-user-emacs-file (file-name-concat "images" "close.svg"))
+	   :ascent center)
+    (text " x "))
+  "Icon for closing the clicked tab."
+  ;; The version here is a bit silly
+  :scale 0.75
+  :version "29.1"
+  :help-echo "Click to close tab")
 
 (tab-bar-mode 1)
 
