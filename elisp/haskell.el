79a04354229463187ca1e17e39a956a9a9c0e685
Jonathan Arnett <jonarnett90@gmail.com>
2023-12-03 00:26:33 -0500

    Fix symbol indentation

--- elisp/haskell.el
+++ elisp/haskell.el
@@ -8,11 +8,11 @@
   (setq haskell-process-type 'stack-ghci)
   :hook
   (haskell-mode . (lambda () (setq prettify-symbols-alist '(("<-" . 8592)
-						       ("->" . 8594)
-						       ("=>" . 8658)
-						       ("/=" . 8800)
-						       ("<=" . 8804)
-						       (">=" . 8805))))))
+							    ("->" . 8594)
+							    ("=>" . 8658)
+							    ("/=" . 8800)
+							    ("<=" . 8804)
+							    (">=" . 8805))))))
 
 (provide 'haskell)
 ;;; haskell.el ends here
