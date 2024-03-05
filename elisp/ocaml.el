01190c5134f8cc486c9b1b26438764eb8776c0eb
Jonathan Arnett <jonarnett90@gmail.com>
2024-03-05 12:58:01 -0500

    Small updates to OCaml config

--- elisp/ocaml.el
+++ elisp/ocaml.el
@@ -1,7 +1,9 @@
 ;;; ocaml --- My OCaml config
 ;;; Commentary:
 ;;; Code:
+
 (use-package tuareg)
+
 (use-package reason-mode)
 
 (let ((opam-share (ignore-errors (car (process-lines "opam" "var" "share")))))
@@ -12,14 +14,21 @@
     (ignore-errors (require 'dune-watch))
     (ignore-errors (require 'ocamlformat))
     (ignore-errors (require 'ocp-indent))
+    (ignore-errors (require 'utop))
     ;; Register Merlin
     (autoload 'merlin-mode "merlin" nil t nil)
+    ;; Register Tuareg
+    (autoload 'tuareg-mode "tuareg-site-file" nil t nil)
     ;; Automatically start it in OCaml buffers
     (add-hook 'tuareg-mode-hook 'merlin-mode t)
     (add-hook 'caml-mode-hook 'merlin-mode t)
     (add-hook 'reason-mode-hook 'merlin-mode t)
     ;; Use opam switch to lookup ocamlmerlin binary
-    (setq merlin-command 'opam)))
+    (setq merlin-command 'opam)
+    ;; To easily change opam switches within a given Emacs session, you can
+    ;; install the minor mode https://github.com/ProofGeneral/opam-switch-mode
+    ;; and use one of its "OPSW" menus.
+    ))
 
 (provide 'ocaml)
 ;;; ocaml.el ends here
